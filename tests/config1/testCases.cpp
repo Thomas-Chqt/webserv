176db000d4b89584565c6e8e26d5744975da0272
Thomas <tchoquet@student.42tokyo.jp>
2024-05-07 20:43:17 +0900

    Update testCases.cpp with new test cases

--- tests/config1/testCases.cpp
+++ tests/config1/testCases.cpp
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 01:55:03 by tchoquet          #+#    #+#             */
-/*   Updated: 2024/05/07 20:34:03 by tchoquet         ###   ########.fr       */
+/*   Updated: 2024/05/07 20:41:21 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,25 @@ TEST_F(Config1, lowerCaseMethod)
     "get / HTTP/1.1\r\n"
     "Host: localhost\r\n"
     "\r\n";
+
+    std::pair<bool, webserv::HTTPResponse> webservResponse = getResponse(request, 8080);
+
+    #ifdef NGINX_PATH
+        std::pair<bool, webserv::HTTPResponse> nginxResponse = getResponse(request, 8090);
+    #endif // NGINX_PATH
+
+    #ifdef NGINX_PATH
+        EXPECT_EQ(webservResponse.first, nginxResponse.first);
+        EXPECT_EQ(webservResponse.second.statusDescription, nginxResponse.second.statusDescription);
+    #else
+        EXPECT_TRUE(webservResponse.first);
+        EXPECT_EQ(webservResponse.second.statusDescription, "Bad Request");
+    #endif // NGINX_PATH
+}
+
+TEST_F(Config1, badRequest)
+{
+    const std::string request = "BAD REQUEST";
 
     std::pair<bool, webserv::HTTPResponse> webservResponse = getResponse(request, 8080);
 
